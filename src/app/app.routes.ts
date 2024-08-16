5c81bc64c49ad0c6b7d63108313e21948a5d0f12
Edinson Gomez <edinson.gomez@akc.co>
2024-08-16 11:44:04 -0500

    uodate routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 
 // menu.routes.ts
 export const routes: Routes = [
-    { path: '', loadComponent: () => import('./app.component').then((x) => x.AppComponent) },
+    { path: 'home', loadComponent: () => import('./app.component').then((x) => x.AppComponent) },
     // Other routes...
   ];
   
