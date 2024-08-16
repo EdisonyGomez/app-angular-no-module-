cd37b647b662b26110e3473231537a2029aafc06
Edinson Gomez <edinson.gomez@akc.co>
2024-08-16 11:38:41 -0500

    se comfigura la ruta principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+// menu.routes.ts
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./app.component').then((x) => x.AppComponent) },
+    // Other routes...
+  ];
+  
