1a944ae2c575d10d2e248f3493cede5bb1b9571f
Edinson Gomez <edinson.gomez@akc.co>
2024-08-20 08:10:44 -0500

    Se añaden estilos de carga de imagenes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -9,17 +9,17 @@ export class ApiService {
 
   RAM_API = 'https://rickandmortyapi.com/api/';
 
-// Se inyecta el cliente http para realizar la comuicación con el API
+  // Se inyecta el cliente http para realizar la comuicación con el API
   constructor(private http: HttpClient) { }
 
   // Servicio encargado de comunicarse con el API de Rick and Morty 
   // y extraer los personajes
-  getData(): Observable<Character[]>{
+  getData(): Observable<Character[]> {
     return this.http.get<{ results: Character[] }>(`${this.RAM_API}/character/`).pipe(
       map(response => response.results.map((character: Character) => ({
         name: character.name,
         image: character.image,
-      })))
+      }))),
     )
   }
 }
